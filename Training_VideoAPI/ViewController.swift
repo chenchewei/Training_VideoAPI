1afd587b5a339c689df8bd137789197552f3fd01
ChenCheWei <gunattack1120@gmail.com>
2020-06-26 19:26:13 +0800

    Initializing project

---
+++ Training_VideoAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training_VideoAPI
+//
+//  Created by mmslab-mini on 2020/6/26.
+//  Copyright © 2020 mmslab-mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
